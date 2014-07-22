a85ab7967a61e527c975f290706a3ab1d82a0323
Lee Safranek <L@d207-023-204-126.wireless.sfu.ca>
2014-07-22 15:37:30 -0700

    yipp

--- learn.m
+++ learn.m
@@ -5,7 +5,7 @@
 %first load the data
 
 %filename
-fn = 'train.csv';
+fn = 'train-edited.csv';
 data = import_KC(fn);
 
 
@@ -13,7 +13,7 @@ data = import_KC(fn);
 %    Parch,Ticket,Fare,Cabin,Embarked
 
 %some of the columns are useless
-useful_columns = [2 3 5 6 7 8 9 10 12];
+useful_columns = [2 3 5 6 7 8 10 12];
 
 %some of the columns are non numerical
 
@@ -25,16 +25,17 @@ data12 = data{12};
 
 
 %%%%%%%
-% Male = 1     Female = 0
+% Male = 1      Female = 0
 % 
-% S = 1   C = 2    Q = 3
-%
+% S = 1    C = 2     Q = 3
+% 
+% 
 %%%%%%%
 
 
 for i = 1:m
     
-    if strcmp(data5{i}, 'Male');
+    if strcmp(data5{i}, 'male');
         data5{i} = '1';
     else
         data5{i} = '0';
@@ -57,7 +58,69 @@ newdata = newdata(useful_columns);
 
 M = zeros(m,size(newdata,2));
 for i = 1:size(newdata,2)
-    i
     derp = cellfun(@str2num,newdata{i});
     M(:,i) = derp;
 end
+
+
+%% Set up machine learning stuffs
+
+%target var is survival
+y = M(:,1);
+
+%features are remaining stuffs
+X = M(:,2:end);
+
+%center data;
+X = (X - repmat(mean(X),m,1))./repmat(std(X),m,1);
+
+% SPLIT INTO TRAINING AND TESTING DATA
+
+m = size(y,1);
+
+%build training data
+mtest = floor(8/10*m);
+
+r = randperm(m);
+
+Xtrain = X(r(1:mtest),:);
+ytrain = y(r(1:mtest));
+
+Xtest = X(r(mtest+1:end),:);
+ytest = y(r(mtest+1:end));
+
+
+
+%% LINEAR REGRESSION
+
+bigX = [ones(mtest,1) Xtrain];
+beta = bigX \ ytrain;
+
+%tset
+yhat = [ones(m-mtest,1) Xtest]*beta;
+yhat = round(yhat);
+
+%find percent of misclassified
+misclass = sum(yhat~=ytest);
+err_rat_ls = misclass/numel(ytest);
+
+%rss
+err = 1/sqrt(numel(ytest))*norm(yhat - ytest);
+
+
+%% DO QDA AND LDA
+
+%LDA
+yhat_lda = classify(Xtest,Xtrain,ytrain,'linear');
+
+%QDA
+yhat_qda = classify(Xtest,Xtrain,ytrain,'quadratic');
+
+
+%find percent of misclassified
+misclass = sum(yhat_lda~=ytest);
+err_rat_lda = misclass/numel(ytest);
+
+%find percent of misclassified
+misclass = sum(yhat_qda~=ytest);
+err_rat_qda = misclass/numel(ytest);
